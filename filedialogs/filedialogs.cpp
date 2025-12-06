df217706f8b095be5bfcbcb3ffe2d2e24b080e6d
high on tantor <josephvenable@verizon.net>
2025-12-06 03:18:45 -0500

    Add files via upload

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -775,7 +775,9 @@ namespace {
           }
         }
       } else {
-        SDL_GetWindowPosition(window, &startx, &starty);
+        if (!ngs::fs::environment_get_variable("IMGUI_DIALOG_PARENT").empty()) {
+          SDL_GetWindowPosition(window, &startx, &starty);
+        }
       }
       #elif ((defined(__linux__) && !defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)) || defined(__sun))
       if (!ngs::fs::environment_get_variable("IMGUI_DIALOG_PARENT").empty()) {
