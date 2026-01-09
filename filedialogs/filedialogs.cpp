bf1b67e6644b104ea21e2adeec1799e5c25c341b
high on tantor <josephvenable@verizon.net>
2026-01-09 01:47:44 -0500

    Update filedialogs.cpp

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -308,7 +308,7 @@ namespace {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD);
     SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
     SDL_WindowFlags windowFlags = (SDL_WindowFlags)(
-    ((ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN").empty()) ? SDL_WINDOW_FULLSCREEN : 0) |
+    ((ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") == std::to_string(1)) ? SDL_WINDOW_FULLSCREEN : 0) |
     ((ngs::fs::environment_get_variable("IMGUI_DIALOG_PARENT").empty()) ? SDL_WINDOW_ALWAYS_ON_TOP : 0) |
     SDL_WINDOW_UTILITY | SDL_WINDOW_HIDDEN | SDL_WINDOW_BORDERLESS);
     window = SDL_CreateWindow(title.c_str(), IFD_DIALOG_WIDTH, IFD_DIALOG_HEIGHT, windowFlags);
@@ -316,11 +316,6 @@ namespace {
     if (ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") != std::to_string(1))
     SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
     dialog = window;
-    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") == std::to_string(1)) {
-      SDL_SetWindowFullscreenMode(window, nullptr);
-      SDL_SetWindowFullscreen(window, true);
-      SDL_SyncWindow(window);
-    }
     renderer = SDL_CreateRenderer(window, nullptr);
     if (renderer == nullptr) return "";
     const char *rendererName = SDL_GetRendererName(renderer);
