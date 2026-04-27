e395dce19acbb7cfc6f2609efe8891f5e20507c9
high on tantor <josephvenable@verizon.net>
2026-04-27 00:25:43 -0400

    Update filesystem.cpp

--- filedialogs/filesystem.cpp
+++ filedialogs/filesystem.cpp
@@ -32,6 +32,7 @@
 #include <random>
 #include <thread>
 
+#include <cstdint>
 #include <climits>
 #include <cstdlib>
 #include <cstring>
@@ -53,7 +54,11 @@
 #else
 #if defined(__APPLE__) && defined(__MACH__)
 #include <sysdir.h>
+#include <TargetConditionals.h>
+#if (defined(TARGET_OS_OSX) && TARGET_OS_OSX)
 #include <libproc.h>
+#include <mach-o/dyld.h>
+#endif
 #elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
 #include <sys/sysctl.h>
 #if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)
@@ -71,8 +76,10 @@ using std::wstring;
 #endif
 
 using std::string;
+using std::stringstream;
 using std::vector;
 using std::size_t;
+using std::uint32_t;
 
 namespace ngs::fs {
 
@@ -216,7 +223,7 @@ namespace ngs::fs {
 
     vector<string> string_split(string str, char delimiter) {
       vector<string> vec;
-      std::stringstream sstr(str);
+      stringstream sstr(str);
       string tmp;
       while (std::getline(sstr, tmp, delimiter)) {
         message_pump();
@@ -507,21 +514,31 @@ namespace ngs::fs {
     string path;
     #if defined(_WIN32)
     wchar_t buffer[MAX_PATH];
-    if (GetModuleFileNameW(nullptr, buffer, sizeof(buffer)) != 0) {
+    if (GetModuleFileNameW(nullptr, buffer, sizeof(buffer))) {
       wchar_t exe[MAX_PATH];
       if (_wfullpath(exe, buffer, MAX_PATH)) {
         path = narrow(exe);
       }
     }
-    #elif (defined(__APPLE__) && defined(__MACH__))
-    char exe[PROC_PIDPATHINFO_MAXSIZE];
-    if (proc_pidpath(getpid(), exe, sizeof(exe)) > 0) {
+    #elif (defined(__APPLE__) && defined(__MACH__) && defined(TARGET_OS_OSX) && TARGET_OS_OSX)
+    char exe[PATH_MAX];
+    uint32_t size = sizeof(exe);
+    if (!_NSGetExecutablePath(exe, &size)) {
       char buffer[PATH_MAX];
       if (realpath(exe, buffer)) {
         path = buffer;
       }
     }
-    #elif (defined(__linux__) && !defined(__ANDROID__))
+    if (path.empty()) {
+      char exe[PROC_PIDPATHINFO_MAXSIZE];
+      if (proc_pidpath(getpid(), exe, sizeof(exe)) > 0) {
+        char buffer[PATH_MAX];
+        if (realpath(exe, buffer)) {
+          path = buffer;
+        }
+      }
+    }
+    #elif defined(__linux__)
     char exe[PATH_MAX];
     if (realpath("/proc/self/exe", exe)) {
       path = exe;
@@ -533,11 +550,11 @@ namespace ngs::fs {
     mib[1] = KERN_PROC;
     mib[2] = KERN_PROC_PATHNAME;
     mib[3] = -1;
-    if (sysctl(mib, 4, nullptr, &len, nullptr, 0) == 0) {
+    if (!sysctl(mib, 4, nullptr, &len, nullptr, 0)) {
       string strbuff;
       strbuff.resize(len, '\0');
       char *exe = strbuff.data();
-      if (sysctl(mib, 4, exe, &len, nullptr, 0) == 0) {
+      if (!sysctl(mib, 4, exe, &len, nullptr, 0)) {
         char buffer[PATH_MAX];
         if (realpath(exe, buffer)) {
           path = buffer;
@@ -551,11 +568,11 @@ namespace ngs::fs {
     mib[1] = KERN_PROC_ARGS;
     mib[2] = -1;
     mib[3] = KERN_PROC_PATHNAME;
-    if (sysctl(mib, 4, nullptr, &len, nullptr, 0) == 0) {
+    if (!sysctl(mib, 4, nullptr, &len, nullptr, 0)) {
       string strbuff;
       strbuff.resize(len, '\0');
       char *exe = strbuff.data();
-      if (sysctl(mib, 4, exe, &len, nullptr, 0) == 0) {
+      if (!sysctl(mib, 4, exe, &len, nullptr, 0)) {
         char buffer[PATH_MAX];
         if (realpath(exe, buffer)) {
           path = buffer;
@@ -578,7 +595,7 @@ namespace ngs::fs {
             fallback:
             char buffer[PATH_MAX];
             if (!stat(exe.c_str(), &st) && (st.st_mode & S_IXUSR) &&
-              (st.st_mode & S_IFREG) && realpath(exe.c_str(), buffer) &&
+              S_ISREG(st.st_mode) && realpath(exe.c_str(), buffer) &&
               st.st_dev == (dev_t)kif[i].va_fsid && st.st_ino == (ino_t)kif[i].va_fileid) {
               res = buffer;
             }
@@ -597,23 +614,30 @@ namespace ngs::fs {
       kvm_close(kd);
       return res;
     };
-    int mib[4];
-    char **cmd = nullptr;
-    size_t len = 0;
-    vector<string> buffer;
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_PROC_ARGS;
-    mib[2] = getpid();
-    mib[3] = KERN_PROC_ARGV;
+    auto cppstr_getenv = [](string name) {
+      const char *cresult = getenv(name.c_str());
+      string result = cresult ? cresult : "";
+      return result;
+    };
+    int cntp = 0;
+    kvm_t *kd = nullptr;
+    kinfo_proc *proc_info = nullptr;
+    std::vector<string> buffer;
     bool error = false, retried = false;
-    if (sysctl(mib, 4, nullptr, &len, nullptr, 0) == 0) {
-      if ((cmd = (char **)malloc(len))) {
-        if (sysctl(mib, 4, cmd, &len, nullptr, 0) == 0) {
-          buffer.push_back(cmd[0]);
+    kd = kvm_openfiles(nullptr, nullptr, nullptr, KVM_NO_FILES, nullptr);
+    if (!kd) {
+      path.clear();
+      return path;
+    }
+    if ((proc_info = kvm_getprocs(kd, KERN_PROC_PID, getpid(), sizeof(struct kinfo_proc), &cntp))) {
+      char **cmd = kvm_getargv(kd, proc_info, 0);
+      if (cmd) {
+        for (int i = 0; cmd[i]; i++) {
+          buffer.push_back(cmd[i]);
         }
-        free(cmd);
       }
     }
+    kvm_close(kd);
     if (!buffer.empty()) {
       string argv0;
       if (!buffer[0].empty()) {
@@ -624,11 +648,11 @@ namespace ngs::fs {
           argv0 = buffer[0];
           path = is_exe(argv0);
         } else if (slash_pos == string::npos || slash_pos > colon_pos) { 
-          string penv = environment_get_variable("PATH");
+          string penv = cppstr_getenv("PATH");
           if (!penv.empty()) {
             retry:
             string tmp;
-            std::stringstream sstr(penv);
+            stringstream sstr(penv);
             while (std::getline(sstr, tmp, ':')) {
               argv0 = tmp + "/" + buffer[0];
               path = is_exe(argv0);
@@ -643,7 +667,7 @@ namespace ngs::fs {
           if (path.empty() && !retried) {
             retried = true;
             penv = "/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/local/bin:/usr/local/sbin";
-            string home = environment_get_variable("HOME");
+            string home = cppstr_getenv("HOME");
             if (!home.empty()) {
               penv = home + "/bin:" + penv;
             }
@@ -651,15 +675,15 @@ namespace ngs::fs {
           }
         }
         if (path.empty() && slash_pos > 0) {
-          string pwd = environment_get_variable("PWD");
+          string pwd = cppstr_getenv("PWD");
           if (!pwd.empty()) {
             argv0 = pwd + "/" + buffer[0];
             path = is_exe(argv0);
           }
           if (path.empty()) {
-            string cwd = directory_get_current_working();
-            if (!cwd.empty()) {
-              argv0 = cwd + "/" + buffer[0];
+            char cwd[PATH_MAX];
+            if (getcwd(cwd, PATH_MAX)) {
+              argv0 = string(cwd) + "/" + buffer[0];
               path = is_exe(argv0);
             }
           }
@@ -668,21 +692,26 @@ namespace ngs::fs {
       if (path.empty() && !error) {
         error = true;
         buffer.clear();
-        string underscore = environment_get_variable("_");
+        string underscore = cppstr_getenv("_");
         if (!underscore.empty()) {
           buffer.push_back(underscore);
           goto fallback;
         }
       }
     }
-    if (!path.empty()) {
-      errno = 0;
-    }
     #elif defined(__sun)
     char exe[PATH_MAX];
+    const char *execname = getexecname();
+    if (execname) {
+      if (realpath(execname, exe)) {
+        path = exe;
+        goto finish;
+      }
+    }
     if (realpath("/proc/self/path/a.out", exe)) {
       path = exe;
     }
+    finish:
     #endif
     return path;
   }
