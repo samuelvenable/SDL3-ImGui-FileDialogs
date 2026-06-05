ca9b977c593c35a0b5357ced476f56359b0cfde5
high on tantor <josephvenable@verizon.net>
2026-06-05 00:35:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd "${0%/*}";
+chmod u+x "./filedialogs/build.sh";
 if [ `uname` = "Darwin" ]; then
   sudo xcode-select --install;
   if [ -f "/opt/local/bin/port" ]; then
     "./filedialogs/build.sh";
   else
     echo "MacPorts installation not found! Please download and install MacPorts first from www.macports.org";
-  fi
+  fi;
 else
   "./filedialogs/build.sh";
-fi
+fi;
